02cef42d45bc28a3781d576df312740357b8306d
Your Name <you@example.com>
2022-10-27 14:54:26 -0300

    deploying models updated

--- run_model.sh
+++ run_model.sh
@@ -1,4 +1,8 @@
 # variable
-data=$(date +'%Y-%m-d%T%H:%M:%S')
+data=$(date +'%Y-%m-%d')
 
-papermill src/models/insiders_clustering_deploy.ipynb reports/insiders_clustering_deploy.ipynb
+# path
+path='C:\\Users\diego\OneDrive\Área de Trabalho\projects\insiders_clustering'
+path_to_envs='/c/Users/diego/anaconda3/Scripts'
+
+$path_to_envs/papermill $path/src/models/insiders_clustering_deploy.ipynb $path/reports/insiders_clustering_deploy_$data.ipynb
